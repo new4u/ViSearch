fd4a030c660637d642e28dd5f729c0edd661b207
Adam <zhangpei.adam@gmail.com>
2022-12-15 10:02:15 +0000

    tampermonkey userscript auto update

---
+++ Debug/ViSearchDebug.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       本爷有空
+// @connect    google.com
+// @connect    google.com.hk
+// @connect    google.com.jp
+// @include    *://encrypted.google.*/search*
+// @include    *://*.google*/search*
+// @include    *://*.google*/webhp*
+// @match        *www.google.com*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=codechef.com
+// @require     https://d3js.org/d3.v4.js
+// @require     http://cdn.bootcss.com/jquery/2.1.4/jquery.min.js
+// @require     http://cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js
+// @resource    http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css
+// @grant        none
+// @require     https://github.com/new4u/ViSearch/blob/68238effb8f81c4b4a86136dbbf0d4c9c774b618/ViSearch.js#L3
+// @updateURL   
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();

5e89d95ef7837417cb101624046863320f2093a0
Adam <zhangpei.adam@gmail.com>
2023-03-18 07:21:04 +0800

    Update ViSearch.user.js

--- ViSearch.user.js
+++ ViSearch.user.js
@@ -29,7 +29,7 @@
 // @grant        none
 // @copyright    2015-2023, new4u
 // @license      GPL-3.0-only
-// @description  Beyond the ChatGPT/AI with eyes.ViSearch as the Lightweight alternatives to ChatGPT,has the potential to be even more intuitive than ChatGPT in the future.
+// @description  Beyond the ChatGPT/AI with eyes.ViSearch as the Free/Lightweight alternatives to ChatGPT,has the potential to be even more intuitive than ChatGPT in the future.
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw
 // @note         2023-2-21-v1.117 发布迁移到userscript以来第一个正式版本
 // @note         2016 Java -> 2019 Node.js -> 2023-2-09 各种各样的历史更新记录，从一个版本迭代到另一个版本
